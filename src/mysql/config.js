ab3da041815934c04a10122d84c440fa3df7239a
gxd3 <guoxiaodong167@gmail.com>
2023-06-05 21:06:43 +0800

    feat: 保存表结构

--- src/mysql/config.js
+++ src/mysql/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'root',
     database: 'techat',
     connectionLimit: 10,
     queueLimit: 100
